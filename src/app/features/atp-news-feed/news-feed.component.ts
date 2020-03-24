675debff28f6726fdbae7684a9fb019d267ae12c
M Aleem <aleem604@gmail.com>
2020-03-24 22:27:50 +0500

    profile fixes

--- src/app/features/atp-news-feed/news-feed.component.ts
+++ src/app/features/atp-news-feed/news-feed.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap';
+import * as jquery from 'jquery';
 
 @Component({
-  selector: 'atp-news-feed',
-  templateUrl: './news-feed.component.html',
+    selector: 'atp-news-feed',
+    templateUrl: './news-feed.component.html',
     styleUrls: ['./news-feed.component.scss'],
     styles: [
         `
@@ -25,133 +27,166 @@ import { Component, OnInit } from '@angular/core';
     ]
 })
 export class NewsFeedComponent implements OnInit {
-  max: number = 5;
-  rate: number = 4;
-  isReadonly: boolean = true;
-  isSubmit = true;
-  news = [
-    {
-      avatar: [{ src: '../../../assets/atp/habitat-for-humanity-logo.png', alt: 'avatar' }],
-      name: 'Habbit for Humanity',
-      desc: 'Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.Nullam dictum felis eu pede mollis pretium.Integer tincidunt.Cras dapibus.Vivamus elementum semper nisi.Aenean vulputate eleifend tellus.Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.Phasellus viverra nulla ut metus varius laoreet.Quisque rutrum.Aenean imperdiet.',
-      time: '30 m',
-      images: [
-        {
-          src: '../../../assets/atp/presbyterian-women.jpg', alt: 'presbyterian women'
-        },
-        {
-          src: '../../../assets/atp/presbyterian-women2.jpg', alt: 'presbyterian 2 women'
-        }
-      ],
-      rating: 2,
-      likes: 10
-    },
-    {
-      avatar: [{ src: '../../../assets/atp/habitat-for-humanity-logo.png', alt: 'avatar' }],
-      name: 'Habbit for Humanity',
-      desc: 'Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.Nullam dictum felis eu pede mollis pretium.Integer tincidunt.Cras dapibus.Vivamus elementum semper nisi.Aenean vulputate eleifend tellus.Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.Phasellus viverra nulla ut metus varius laoreet.Quisque rutrum.Aenean imperdiet.',
-      time: '2.5 Hr',
-      images: [
+    modalRef: BsModalRef;
+    max: number = 5;
+    rate: number = 4;
+    isReadonly: boolean = true;
+    isSubmit = true;
 
-      ],
-      rating: 3,
-      likes: 2
-    },
-    {
-      avatar: [{ src: '../../../assets/atp/wwf-logo.png', alt: 'avatar' }],
-      name: 'WWF12',
-      desc: 'Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.Nullam dictum felis eu pede mollis pretium.Integer tincidunt.Cras dapibus.Vivamus elementum semper nisi.Aenean vulputate eleifend tellus.Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.Phasellus viverra nulla ut metus varius laoreet.Quisque rutrum.Aenean imperdiet.',
-      time: '45 m',
-      images: [
+    exampleData = [
         {
-          src: '../../../assets/atp/wwf.jpg', alt: 'wwf women'
-        }
-      ],
-      rating: 5,
-      likes: 12
-    },
-    {
-      avatar: [{ src: '../../../assets/atp/presbyterian-women-logo.png', alt: 'avatar' }],
-      name: 'John Doe',
-      desc: 'Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.Nullam dictum felis eu pede mollis pretium.Integer tincidunt.Cras dapibus.Vivamus elementum semper nisi.Aenean vulputate eleifend tellus.Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.Phasellus viverra nulla ut metus varius laoreet.Quisque rutrum.Aenean imperdiet.',
-      time: '30 m',
-      images: [
+            id: 'opt1',
+            text: 'Options 1'
+        },
         {
-          src: '../../../assets/atp/presbyterian-women.jpg', alt: 'presbyterian women'
+            id: 'opt2',
+            text: 'Options 2'
+        },
+        {
+            id: 'opt3',
+            text: 'Options 3'
+        },
+        {
+            id: 'opt4',
+            text: 'Options 4'
         }
-      ],
-      rating: 3,
-      likes: 10
-    },
+    ];
 
-    {
-      avatar: [{ src: '../../../assets/atp/habitat-for-humanity-logo.png', alt: 'avatar' }],
-      name: 'Habbit for Humanity',
-      desc: 'Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.Nullam dictum felis eu pede mollis pretium.Integer tincidunt.Cras dapibus.Vivamus elementum semper nisi.Aenean vulputate eleifend tellus.Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.Phasellus viverra nulla ut metus varius laoreet.Quisque rutrum.Aenean imperdiet.',
-      time: '30 m',
-      images: [
-        {
-          src: '../../../assets/atp/presbyterian-women.jpg', alt: 'presbyterian women'
-        },
-        {
-          src: '../../../assets/atp/presbyterian-women2.jpg', alt: 'presbyterian 2 women'
-        }
-      ],
-      rating: 1,
-      likes: 2
-    },
-    {
-      avatar: [{ src: '../../../assets/atp/habitat-for-humanity-logo.png', alt: 'avatar' }],
-      name: 'Habbit for Humanity',
-      desc: 'Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.Nullam dictum felis eu pede mollis pretium.Integer tincidunt.Cras dapibus.Vivamus elementum semper nisi.Aenean vulputate eleifend tellus.Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.Phasellus viverra nulla ut metus varius laoreet.Quisque rutrum.Aenean imperdiet.',
-      time: '30 m',
-      images: [
-        {
-          src: '../../../assets/atp/presbyterian-women.jpg', alt: 'presbyterian women'
-        },
-        {
-          src: '../../../assets/atp/presbyterian-women2.jpg', alt: 'presbyterian 2 women'
-        }
-      ],
-      rating: 4,
-      likes: 4
-    },
-    {
-      avatar: [{ src: '../../../assets/atp/habitat-for-humanity-logo.png', alt: 'avatar' }],
-      name: 'Habbit for Humanity',
-      desc: 'Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.Nullam dictum felis eu pede mollis pretium.Integer tincidunt.Cras dapibus.Vivamus elementum semper nisi.Aenean vulputate eleifend tellus.Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.Phasellus viverra nulla ut metus varius laoreet.Quisque rutrum.Aenean imperdiet.',
-      time: '30 m',
-      images: [
-        {
-          src: '../../../assets/atp/presbyterian-women.jpg', alt: 'presbyterian women'
-        },
-        {
-          src: '../../../assets/atp/presbyterian-women2.jpg', alt: 'presbyterian 2 women'
-        }
-      ],
-      rating: 4,
-      likes: 4
-    },
-    {
-      avatar: [{ src: '../../../assets/atp/habitat-for-humanity-logo.png', alt: 'avatar' }],
-      name: 'Habbit for Humanity',
-      desc: 'Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.Nullam dictum felis eu pede mollis pretium.Integer tincidunt.Cras dapibus.Vivamus elementum semper nisi.Aenean vulputate eleifend tellus.Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.Phasellus viverra nulla ut metus varius laoreet.Quisque rutrum.Aenean imperdiet.',
-      time: '30 m',
-      images: [
-        {
-          src: '../../../assets/atp/presbyterian-women.jpg', alt: 'presbyterian women'
-        },
-        {
-          src: '../../../assets/atp/presbyterian-women2.jpg', alt: 'presbyterian 2 women'
-        }
-      ],
-      rating: 4,
-      likes: 4
-    },
-  ];
-  constructor() { }
+    options = {
+        multiple: false,
+        theme: 'classic',
+        closeOnSelect: false
+    }
 
-  ngOnInit() {
-  }
+
+    news = [
+        {
+            avatar: [{ src: '../../../assets/atp/habitat-for-humanity-logo.png', alt: 'avatar' }],
+            name: 'Habbit for Humanity',
+            desc: 'Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.Nullam dictum felis eu pede mollis pretium.Integer tincidunt.Cras dapibus.Vivamus elementum semper nisi.Aenean vulputate eleifend tellus.Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.Phasellus viverra nulla ut metus varius laoreet.Quisque rutrum.Aenean imperdiet.',
+            time: '30 m',
+            images: [
+                {
+                    src: '../../../assets/atp/presbyterian-women.jpg', alt: 'presbyterian women'
+                },
+                {
+                    src: '../../../assets/atp/presbyterian-women2.jpg', alt: 'presbyterian 2 women'
+                }
+            ],
+            rating: 2,
+            likes: 10
+        },
+        {
+            avatar: [{ src: '../../../assets/atp/habitat-for-humanity-logo.png', alt: 'avatar' }],
+            name: 'Habbit for Humanity',
+            desc: 'Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.Nullam dictum felis eu pede mollis pretium.Integer tincidunt.Cras dapibus.Vivamus elementum semper nisi.Aenean vulputate eleifend tellus.Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.Phasellus viverra nulla ut metus varius laoreet.Quisque rutrum.Aenean imperdiet.',
+            time: '2.5 Hr',
+            images: [
+
+            ],
+            rating: 3,
+            likes: 2
+        },
+        {
+            avatar: [{ src: '../../../assets/atp/wwf-logo.png', alt: 'avatar' }],
+            name: 'WWF12',
+            desc: 'Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.Nullam dictum felis eu pede mollis pretium.Integer tincidunt.Cras dapibus.Vivamus elementum semper nisi.Aenean vulputate eleifend tellus.Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.Phasellus viverra nulla ut metus varius laoreet.Quisque rutrum.Aenean imperdiet.',
+            time: '45 m',
+            images: [
+                {
+                    src: '../../../assets/atp/wwf.jpg', alt: 'wwf women'
+                }
+            ],
+            rating: 5,
+            likes: 12
+        },
+        {
+            avatar: [{ src: '../../../assets/atp/presbyterian-women-logo.png', alt: 'avatar' }],
+            name: 'John Doe',
+            desc: 'Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.Nullam dictum felis eu pede mollis pretium.Integer tincidunt.Cras dapibus.Vivamus elementum semper nisi.Aenean vulputate eleifend tellus.Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.Phasellus viverra nulla ut metus varius laoreet.Quisque rutrum.Aenean imperdiet.',
+            time: '30 m',
+            images: [
+                {
+                    src: '../../../assets/atp/presbyterian-women.jpg', alt: 'presbyterian women'
+                }
+            ],
+            rating: 3,
+            likes: 10
+        },
+
+        {
+            avatar: [{ src: '../../../assets/atp/habitat-for-humanity-logo.png', alt: 'avatar' }],
+            name: 'Habbit for Humanity',
+            desc: 'Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.Nullam dictum felis eu pede mollis pretium.Integer tincidunt.Cras dapibus.Vivamus elementum semper nisi.Aenean vulputate eleifend tellus.Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.Phasellus viverra nulla ut metus varius laoreet.Quisque rutrum.Aenean imperdiet.',
+            time: '30 m',
+            images: [
+                {
+                    src: '../../../assets/atp/presbyterian-women.jpg', alt: 'presbyterian women'
+                },
+                {
+                    src: '../../../assets/atp/presbyterian-women2.jpg', alt: 'presbyterian 2 women'
+                }
+            ],
+            rating: 1,
+            likes: 2
+        },
+        {
+            avatar: [{ src: '../../../assets/atp/habitat-for-humanity-logo.png', alt: 'avatar' }],
+            name: 'Habbit for Humanity',
+            desc: 'Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.Nullam dictum felis eu pede mollis pretium.Integer tincidunt.Cras dapibus.Vivamus elementum semper nisi.Aenean vulputate eleifend tellus.Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.Phasellus viverra nulla ut metus varius laoreet.Quisque rutrum.Aenean imperdiet.',
+            time: '30 m',
+            images: [
+                {
+                    src: '../../../assets/atp/presbyterian-women.jpg', alt: 'presbyterian women'
+                },
+                {
+                    src: '../../../assets/atp/presbyterian-women2.jpg', alt: 'presbyterian 2 women'
+                }
+            ],
+            rating: 4,
+            likes: 4
+        },
+        {
+            avatar: [{ src: '../../../assets/atp/habitat-for-humanity-logo.png', alt: 'avatar' }],
+            name: 'Habbit for Humanity',
+            desc: 'Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.Nullam dictum felis eu pede mollis pretium.Integer tincidunt.Cras dapibus.Vivamus elementum semper nisi.Aenean vulputate eleifend tellus.Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.Phasellus viverra nulla ut metus varius laoreet.Quisque rutrum.Aenean imperdiet.',
+            time: '30 m',
+            images: [
+                {
+                    src: '../../../assets/atp/presbyterian-women.jpg', alt: 'presbyterian women'
+                },
+                {
+                    src: '../../../assets/atp/presbyterian-women2.jpg', alt: 'presbyterian 2 women'
+                }
+            ],
+            rating: 4,
+            likes: 4
+        },
+        {
+            avatar: [{ src: '../../../assets/atp/habitat-for-humanity-logo.png', alt: 'avatar' }],
+            name: 'Habbit for Humanity',
+            desc: 'Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.Nullam dictum felis eu pede mollis pretium.Integer tincidunt.Cras dapibus.Vivamus elementum semper nisi.Aenean vulputate eleifend tellus.Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.Phasellus viverra nulla ut metus varius laoreet.Quisque rutrum.Aenean imperdiet.',
+            time: '30 m',
+            images: [
+                {
+                    src: '../../../assets/atp/presbyterian-women.jpg', alt: 'presbyterian women'
+                },
+                {
+                    src: '../../../assets/atp/presbyterian-women2.jpg', alt: 'presbyterian 2 women'
+                }
+            ],
+            rating: 4,
+            likes: 4
+        },
+    ];
+    constructor(private modalService: BsModalService) { }
+    ngOnInit() {
+        jquery('.select2-select').select2();
+    }
+    submitContent(template: TemplateRef<any>) {
+        this.modalRef = this.modalService.show(template, { class: 'modal-sm modal-dialog-centered', ignoreBackdropClick: false, keyboard: false });
+        return this.modalRef;
+    }
+
 
 }
